a1de444800069aaea37811a76f164666c0f066ab
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2025-10-03 00:22:32 +0800

    Create Problem6.cpp

---
+++ Activity4/Problem6.cpp
@@ -0,0 +1,11 @@
+/*
+Problem 6: Switch problem 1 to 5
+Combine all problems using switch statement
+
+Sample Input:
+What problem do you want to see? 1
+
+Sample Output:
+*Basic pricing Decision Runs*
+*/
+
