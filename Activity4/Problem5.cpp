8a4aadcfe8398cb5b92c5e760c988a693dc74801
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2025-10-03 00:18:57 +0800

    Create Problem5.cpp

---
+++ Activity4/Problem5.cpp
@@ -0,0 +1,60 @@
+/*
+Problem 5: Player Movement
+
+Problem:
+The player moves depending on the pressed button. Player starts at (0,0).
+
+Task:
+
+Write a program that:
+Lets the player change coordinates depending on the button pressed.
+W =  0, +1
+S = 0, -1
+A = -1,0
+D = +1,0 
+
+Sample Input:
+W
+
+
+Sample Output:
+The location of the player is ( 0, 1)
+*/
+
+//Code:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char choice;
+    
+    cin>> choice;
+    
+    switch(choice) {
+        
+        case 'W':
+            cout<< "The location of the player is (0, 1).";
+        break;
+        
+        case 'S':
+            cout<< "The location of the player is (0, -1).";
+        break;
+        
+        case 'A':
+            cout<< "The location of the player is (-1, 0).";
+        break;
+        
+        case 'D':
+            cout<< "The location of the player is (1, 0).";
+        break;
+        
+        default:
+            cout<< "Invalid input.";
+        break;
+        
+    }
+
+    return 0;
+}
