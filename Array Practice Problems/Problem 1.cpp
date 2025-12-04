e490d065bdbec6bd178c94821342446f2ae53c19
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2025-12-04 16:23:58 +0800

    Create Problem 1.cpp

---
+++ Array Practice Problems/Problem 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] <= arr[i + 1]) {
+            count++;
+        }
+    }
+
+    if (count == n - 1) {
+        cout << "The array is in ascending order." << endl;
+    } else {
+        cout << "The array is not in ascending order." << endl;
+    }
+
+    return 0;
+}
