a81a7f87f267b672b69b1b5570f1bc9afeeab9e5
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2025-12-11 18:20:29 +0800

    Update Problem 1.cpp

--- Array Practice Problems/Problem 1.cpp
+++ Array Practice Problems/Problem 1.cpp
@@ -6,15 +6,15 @@ int main() {
     cout << "Enter number of elements: ";
     cin >> n;
 
-    int arr[n];
+    int numbers[n];
     cout << "Enter elements: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> numbers[i];
     }
 
     int count = 0;
     for (int i = 0; i < n - 1; i++) {
-        if (arr[i] <= arr[i + 1]) {
+        if (numbers[i] <= numbers[i + 1]) {
             count++;
         }
     }
